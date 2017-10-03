1c3029922dbca2f8a0760e71d27a28a52d8558bd
Nico Zhu <v-zhumin@microsoft.com>
2017-10-03 09:38:12 +0800

    Add project files.

---
+++ GridViewFlowTest/FlowItmePanel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+
+namespace GridViewFlowTest
+{
+    public class FlowItmePanel : Panel
+    {
+
+        protected override Size MeasureOverride(Size availableSize)
+        {
+            Size s = base.MeasureOverride(availableSize);
+
+            foreach (UIElement element in this.Children)
+                element.Measure(availableSize);
+            return s;
+        }
+        protected override Size ArrangeOverride(Size finalSize)
+        {
+
+            this.Clip = new RectangleGeometry { Rect = new Rect(0, 0, finalSize.Width, finalSize.Height) };
+
+            foreach (FrameworkElement element in this.Children)
+            {
+
+              //  element.Arrange(new Rect(rectX, rectY, element.DesiredSize.Width, element.DesiredSize.Height));
+            }
+               return finalSize;
+        }
+
+    }
+}
---
+++ GridViewFlowTest/VariableGrid.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Documents;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+
+// The Templated Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234235
+
+namespace GridViewFlowTest
+{
+    public sealed class VariableGrid : GridView
+    {
+        public VariableGrid()
+        {
+            this.DefaultStyleKey = typeof(VariableGrid);
+        }
+        protected override void PrepareContainerForItemOverride(DependencyObject element, object item)
+        {
+            var list = this.ItemsSource as List<string>;      
+            var griditem = element as GridViewItem;
+            for (var t = (list.Count - list.Count % 4); t < list.Count; t++)
+            {
+                if (item as string == list[t])
+                {
+                    if (griditem != null)
+                    {
+                        VariableSizedWrapGrid.SetColumnSpan(griditem, 2);
+                    }
+                }
+            }
+            base.PrepareContainerForItemOverride(element, item);
+        }
+
+        protected override Size MeasureOverride(Size availableSize)
+        {
+            return base.MeasureOverride(availableSize);
+
+
+        }
+        protected override Size ArrangeOverride(Size finalSize)
+        {
+
+            this.Clip = new RectangleGeometry { Rect = new Rect(0, 0, finalSize.Width, finalSize.Height) };
+
+            foreach (FrameworkElement element in this.ItemsPanelRoot.Children)
+            {
+                var list = this.ItemsSource as List<string>;
+              
+                for (var t = ((list.Count - list.Count % 4)); t < list.Count; t++)
+                {
+                   
+                }
+              //  element.Arrange(new Rect(rectX, rectY, element.DesiredSize.Width, element.DesiredSize.Height));
+            }
+                return base.ArrangeOverride(finalSize);
+        }
+       
+    }
+}
